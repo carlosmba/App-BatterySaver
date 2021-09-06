babeb507e22027c5967391b64b036a184f5a4d6e
Carlos Mendoza <carlos.mba19@gmail.com>
2021-09-05 22:34:58 -0400

    agregando mas funciones

---
+++ app/src/main/java/com/mendozasolutions/batterysaver/ui/main/OnClickAppListener.kt
@@ -0,0 +1,7 @@
+package com.mendozasolutions.batterysaver.ui.main
+
+
+interface OnClickAppListener {
+    fun onClickCheckBoxAll(isCheck : Boolean)
+    fun onClickCheckBoxApp(isChecked : Boolean)
+}
