3635009c417a118c5d1c571297c2da5d412b1b9a
Carlos Mendoza <carlos.mba19@gmail.com>
2021-09-05 22:19:57 -0400

    Eliminando archivos innecesarios

--- app/src/main/java/com/mendozasolutions/batterysaver/ui/home/OnClickAppListener.kt
+++
@@ -1,8 +0,0 @@
-package com.mendozasolutions.batterysaver.ui.home
-
-import com.mendozasolutions.batterysaver.data.model.App
-
-interface OnClickAppListener {
-    fun onClickForceCloseApp(listApp : List<App>)
-    fun onClickCheckBoxAll(isCheck : Boolean)
-}
